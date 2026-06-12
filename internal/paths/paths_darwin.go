7b5f503a0feb7b72f1a445988bb1192491ee3583
Sandeep Baynes <sandeep.baynes@gmail.com>
2026-06-12 22:25:41 +0300

    Resolve data root: system path if provisioned, else legacy ~/.byn
    Signed-off-by: Sandeep Baynes <sandeep.baynes@gmail.com>

--- internal/paths/paths_darwin.go
+++ internal/paths/paths_darwin.go
@@ -2,5 +2,9 @@
 
 package paths
 
-func dataDir() string    { return "/Library/Application Support/byn" }
+// systemDataDir is the macOS system-service state path used once an install is
+// provisioned to run the daemon as _byn (owned _byn, 0700).
+func systemDataDir() string { return "/Library/Application Support/byn" }
+
+// socketPath is the provisioned runtime socket under the support dir.
 func socketPath() string { return "/Library/Application Support/byn/daemon.sock" }
